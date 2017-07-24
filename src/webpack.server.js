59c6c8ee3788304a5247de4a3be65aac49ccd1d2
wartoshika <dev@qhun.de>
2017-07-24 20:25:10 +0200

    resolved issue with webpack cfg

--- src/webpack.server.js
+++ src/webpack.server.js
@@ -19,10 +19,6 @@ module.exports = {
         path: path.resolve(__dirname, '../dist'),
         filename: 'server.js'
     },
-    resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['.ts', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
-    },
     devtool: "source-map",
     module: {
         rules: [
